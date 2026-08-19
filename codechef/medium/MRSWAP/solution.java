674117990bcadaedf45a7a54421b988a148f6cde
K.Tanooj Sai <ktanoojsai@gmail.com>
2026-08-19 20:13:05 +0530

    ✅ MRSWAP | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-19

---
+++ codechef/medium/MRSWAP/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            int a[]=new int[2*n];
+            for(int i=0;i<2*)
+	}
+}
