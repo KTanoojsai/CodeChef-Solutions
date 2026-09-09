ec8322c7e7ef8799eaf1b855a13b267eccab08b8
K.Tanooj Sai <ktanoojsai@gmail.com>
2026-09-09 20:05:29 +0530

    ✅ EGGBUY | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

---
+++ codechef/medium/EGGBUY/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int f=sc.nextInt();
+        int xcost=x*12;
+        int ycost=(y*12)+f;
+        System.out.println(Math.min(xcost,ycost));
+	}
+}
