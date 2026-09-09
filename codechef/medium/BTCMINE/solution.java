f860fc5eac973d7dda0e79b778b1c5b68cb10dc5
K.Tanooj Sai <ktanoojsai@gmail.com>
2026-09-09 21:05:23 +0530

    ✅ BTCMINE | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/BTCMINE/solution.java
+++ codechef/medium/BTCMINE/solution.java
@@ -7,6 +7,23 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-        
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int k=0;
+            int days;
+            int receivedCoins=0;
+            int paid=0;
+            int profits=0;
+            while(profits<=0)
+            {
+                day++;
+                k++;
+                receivedCoins=y*(k*k);
+            }
+        }
 	}
 }
