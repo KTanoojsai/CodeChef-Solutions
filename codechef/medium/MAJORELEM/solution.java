83373c079be19a90444e753ad406e5fb294e814e
K.Tanooj Sai <ktanoojsai@gmail.com>
2026-09-15 12:13:11 +0530

    ✅ MAJORELEM | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-15

---
+++ codechef/medium/MAJORELEM/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] arr) {
+        int candidate = 0;
+        int count = 0;
+
+        // Find the possible majority element
+        for (int num : arr) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+}
