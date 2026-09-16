d1093b7147a178635909ebea1d0f57cfdd757e17
K.Tanooj Sai <ktanoojsai@gmail.com>
2026-09-16 20:49:40 +0530

    ✅ FARSWAPLEX | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

---
+++ codechef/medium/FARSWAPLEX/solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int a[]=new int[n];
+            for(int i=0;i<n;i++){
+                a[i]=sc.nextInt();
+            }
+            for(int i=0;i<n-1;i++){
+                if(a[i]<=a[i+1])
+                    continue;
+                if(a[i]>a[i+1]&&(a[i]-a[i+1])>1){
+                    int temp=a[i];
+                    a[i]=a[i+1];
+                    a[i+1]=temp;
+                }
+            }
+            System.out.println(a.toString());
+        }
+	}
+}
