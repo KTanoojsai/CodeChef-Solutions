726e2dcdfc992a2c9257741f57c05b4666295a54
K.Tanooj Sai <ktanoojsai@gmail.com>
2026-09-17 22:41:27 +0530

    ✅ OJJAR39 | Medium | JavaScript | Runtime: N/A | Memory: N/A | 2026-09-17

---
+++ codechef/medium/OJJAR39/solution.js
@@ -0,0 +1,15 @@
+let originalNumbers = [1, 5, 10];
+
+// Create an empty array to store the results
+let doubledNumbers=[];
+// Iterate over the original numbers
+originalNumbers.forEach(function(number){
+    let doubled=number*2;
+    doubledNumbers.push(doubled);
+});
+// Calculate the doubled value
+
+// Add the doubled value to our new array
+
+
+console.log("Doubled:",doubledNumbers );
