db2ea3f6ccf5579f4dce7a5eb38af2f7919f3d10
K.Tanooj Sai <ktanoojsai@gmail.com>
2026-09-19 23:56:27 +0530

    ✅ DPNMAO15 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-19

--- codechef/medium/DPNMAO15/solution.java
+++ codechef/medium/DPNMAO15/solution.java
@@ -26,7 +26,7 @@ class Codechef
             count++;
         }
         for(int i=count;i<Math.max(m,n);i++){
-            if(m<n)
+            if(m>=n)
                 al.add(arr2[i]);
             else    
                 al.add(arr1[i]);
