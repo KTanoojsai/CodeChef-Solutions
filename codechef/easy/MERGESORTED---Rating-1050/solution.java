bedf843e09b74b82f26bace4406cf474343cb0c8
K.Tanooj Sai <ktanoojsai@gmail.com>
2026-09-20 10:46:16 +0530

    ✅ MERGESORTED - Rating 1050 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-20

--- codechef/easy/MERGESORTED---Rating-1050/solution.java
+++ codechef/easy/MERGESORTED---Rating-1050/solution.java
@@ -19,10 +19,9 @@ class Codechef
 		    narr[i]=sc.nextInt();
 		}
 		ArrayList<Integer> al = new ArrayList<>();
-
         int i = 0;
         int j = 0;
-       while (i < m && j < n) {
+        while (i < m && j < n) {
             if (marr[i] <= narr[j]) {
                 al.add(marr[i]);
                 i++;
