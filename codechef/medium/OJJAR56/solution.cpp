9c7f0e33e0ba2e6304a6b94df60712595578684a
K.Tanooj Sai <ktanoojsai@gmail.com>
2026-09-21 14:15:02 +0530

    ✅ OJJAR56 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/medium/OJJAR56/solution.cpp
@@ -0,0 +1,20 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        // add the width here 
+        // add the height here 
+        width: 200,
+        backgroundColor: 'lightblue',
+        color: 'white',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '18px'
+      }}
+    >
+      Styled Box
+    </div>
+  );
+}
+        height: 100,
