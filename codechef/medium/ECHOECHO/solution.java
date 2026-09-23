17df5d83b4ea247e9cf61b775c2e76d607a47e52
Kalluri Tanooj Sai <ktanoojsai@gmail.com>
2026-09-23 20:44:06 +0530

    ✅ ECHOECHO | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

--- codechef/medium/ECHOECHO/solution.java
+++ codechef/medium/ECHOECHO/solution.java
@@ -2,19 +2,14 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-        Scanner sc=new Scanner(System.in);
-        String s=sc.next();
-        char []a=s.toCharArray();
-        int l=1,r=3;
-            if(a[l]==a[r] && a[l+1]==a[r+1])
-                return new String "Yes";
-            else    
-                return new String "No";
-            
-	}
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        if (s.charAt(0) == s.charAt(2) && s.charAt(1) == s.charAt(3)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
 }
