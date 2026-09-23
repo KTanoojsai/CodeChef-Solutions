54e926dff67e68292668432aa9f0ddbc7f64c10b
Kalluri Tanooj Sai <ktanoojsai@gmail.com>
2026-09-23 20:53:22 +0530

    ✅ PGNATK | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-23

---
+++ codechef/medium/PGNATK/solution.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            int k=sc.nextInt();
+            int c=0;
+            while(n>0)
+            {
+                if(n==k)
+                {
+                    c++;
+                    continue;
+                }else
+                {
+                    c++;
+                    n--;
+                    k=2*k;
+                }
+                    
+            }
+            System.out.println(c);
+        }
+
+	}
+}
